f8c879948269a29dfe56739d2eab52b4f3a2f548
Anas <anas@MacBook-Pro-de-Anas.local>
2021-12-01 15:17:06 +0100

    aa

---
+++ src/github.java
@@ -0,0 +1,16 @@
+public class github {
+
+    public static void main(String args[]) {
+
+
+            int nbr, i;
+            nbr = 5;
+
+            System.out.println("La table de multiplication de " + nbr);
+            for (i = 1; i <= 10; i++)
+                System.out.println(nbr + "x" + i + " = " + (nbr * i));
+
+
+    }
+
+}
